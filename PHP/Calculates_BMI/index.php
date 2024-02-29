cf1c6c287a31a68c3b85524bfaa5feabd46dc14b
João Victor Peres Silva <vitorr-joao2222@hotmail.com>
2024-02-28 22:48:56 -0300

    calculates_BMI

---
+++ PHP/Calculates_BMI/index.php
@@ -0,0 +1,38 @@
+<?php 
+
+    // 28/02/24
+
+    /* DESCIPTION
+        Write function bmi that calculates body mass index (bmi = weight / height2).
+
+        if bmi <= 18.5 return "Underweight"
+        if bmi <= 25.0 return "Normal"
+        if bmi <= 30.0 return "Overweight"
+        if bmi > 30 return "Obese"    
+    */
+
+    echo bmi(110, 1.80);
+
+    function bmi(int $weight, float $height): string 
+    {
+        $result = $weight / ($height**2);
+        $body_mass = '';
+
+        switch ($result) {
+            case $result <= 18.5:
+                $body_mass = 'Underwright';
+                break;
+            case $result <= 25:
+                $body_mass = 'Normal';
+                break;
+            case $result <= 30:
+                $body_mass = 'Overweight';
+                break;
+            case $result > 30:
+                $body_mass = 'Obese';
+                break;
+        }
+
+        return $body_mass;
+    }
+?>
---
+++ PHP/Calculates_BMI/tests.php
@@ -0,0 +1,37 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    public function testFixedTests() {
+      $this->assertEquals("Underweight", bmi(50, 1.80));
+      $this->assertEquals("Normal", bmi(81, 1.80));
+      $this->assertEquals("Overweight", bmi(90, 1.80));
+      $this->assertEquals("Obese", bmi(110, 1.80));
+    }
+  
+    private function solution($weight, $height) {
+      $bmi = $weight / $height ** 2;
+
+      if ($bmi <= 18.5) {
+        return "Underweight";
+      } elseif ($bmi <= 25) {
+        return "Normal";
+      } elseif ($bmi <= 30) {
+        return "Overweight";
+      } else {
+        return "Obese";
+      } 
+    }
+  
+    public function testRandomTests() {
+      for ($i = 0; $i < 25; $i++) {
+        $target = rand(15, 35);
+        $height = rand(1.5, 2);
+        $weight = $target * $height * $height;
+        
+        $this->assertEquals($this->solution($weight, $height), bmi($weight, $height), "Testing for weight = $weight, height = $height");
+      }
+    }
+  
+}
